a3a48ac6ca6ebe6faf955da7036a37a30408eaf8
louisbl <louis@hyperfiction.fr>
2013-04-25 12:34:12 +0200

    ARC and min-version 5.0 for iOS

--- project/android.sh
+++ project/android.sh
@@ -1,6 +1,3 @@
 rm -rf "obj"
-rm ../ndll/Android/libHypSystem.so
 echo "Compiling for armv6"
 haxelib run hxcpp Build.xml -Dandroid
-echo "Copying..."
-cp ../ndll/Android/libHypSystem.so ../../../bin/android/bin/libs/armeabi/libHypSystem.so
---
+++ project/iphone.sh
@@ -0,0 +1,8 @@
+﻿rm -rf "obj"
+echo "compiling for armv6"
+haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_CLANG -DOBJC_ARC
+echo "compiling for armv7"
+haxelib run hxcpp Build.xml -Diphoneos -DHXCPP_ARMV7 -DHXCPP_CLANG -DOBJC_ARC
+echo "compiling for simulator"
+haxelib run hxcpp Build.xml -Diphonesim -DHXCPP_CLANG -DOBJC_ARC
+echo "Done ! \n"
