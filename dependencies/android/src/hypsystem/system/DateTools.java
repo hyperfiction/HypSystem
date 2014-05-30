d89bb89dcc4267ed50bca97b0c056fc67d23f9d4
shoebox <shoeboks@gmail.com>
2014-05-31 00:32:57 +0100

    DateTools for Android

--- dependencies/android/src/hypsystem/system/DateTools.java
+++ dependencies/android/src/hypsystem/system/DateTools.java
@@ -1,39 +1,129 @@
 package hypsystem.system;
 
+import java.lang.StringBuffer;
 import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.text.ParseException;
-import java.util.TimeZone;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
-public class DateTools
+class DateTools
 {
+	static final String DATEFORMAT_UTC = "yyyy-MM-dd HH:mm:ss";
+	static final String DATEFORMAT_ISO = "yyyy-MM-dd'T'HH:mm:ssz";
 
-	public static float fromISOStringToMs(String iso)
+	public static float getTimezoneOffset()
 	{
-		Date result = null;
-		try
-		{
-			SimpleDateFormat formatter = new SimpleDateFormat ( "yyyy-MM-dd'T'HH:mm:ssXXX");
-			result = formatter.parse(iso);
-		}
-		catch ( ParseException e )
-		{
-
-		}
-
-		Float time = result == null ? -1 : (float)result.getTime();
-		return time;
-	}	
-
-	public static String timestampToIsoString(float timestamp)
-	{
-		Date date = new Date((long)timestamp);
-		TimeZone tz = TimeZone.getTimeZone("UTC");
-    	DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
-	    df.setTimeZone(tz);
-    	String dateAsIso = df.format(date);
-    	return dateAsIso;
+		TimeZone tz = TimeZone.getDefault();
+		Date now = new Date();
+		float offsetFromUtc = tz.getOffset(now.getTime()) / 1000;
+		return offsetFromUtc;
 	}
 
+	public static float getUTCFullYear(float timestamp)
+	{
+		return getUTC(getDate(timestamp)).get(Calendar.YEAR);
+	}
+
+	public static float getUTCMonth(float timestamp)
+	{
+		return getUTC(getDate(timestamp)).get(Calendar.MONTH);
+	}
+
+	public static float getUTCDay(float timestamp)
+	{
+		return getUTC(getDate(timestamp)).get(Calendar.DAY_OF_WEEK);
+	}
+
+	public static float getUTCDate(float timestamp)
+	{
+		return getUTC(getDate(timestamp)).get(Calendar.DAY_OF_MONTH);
+	}
+
+	public static float getUTCHours(float timestamp)
+	{
+		return getUTC(getDate(timestamp)).get(Calendar.HOUR_OF_DAY);
+	}
+
+	public static float getUTCMinutes(float timestamp)
+	{
+		return getUTC(getDate(timestamp)).get(Calendar.MINUTE );
+	}
+
+	public static float getUTCSeconds(float timestamp)
+	{
+		return getUTC(getDate(timestamp)).get(Calendar.SECOND);
+	}
+
+	public static float getUTCMilliseconds(float timestamp)
+	{
+		return getUTC(getDate(timestamp)).get(Calendar.MILLISECOND);
+	}
+
+	public static String toUTCString(float timestamp)
+	{
+		Calendar utcCalendar = getUTC(getDate(timestamp));
+		DateFormat date = new SimpleDateFormat(DATEFORMAT_UTC);   
+		date.setTimeZone(TimeZone.getTimeZone("GMT")); 
+		String utc = date.format(utcCalendar.getTime()); 
+		return utc;
+	}
+
+	public static String toISOString(float timestamp)
+	{
+		TimeZone tz = TimeZone.getTimeZone("UTC");
+		DateFormat df = new SimpleDateFormat(DATEFORMAT_ISO);
+		df.setTimeZone(tz);
+		String nowAsISO = df.format(new Date());
+		return nowAsISO;
+	}
+
+	static String toIndice(final int value)
+	{
+		if (value > 9)
+			return String.valueOf(value);
+		else
+			return "0" + value;
+	}
+
+	static Calendar getUTC(Date from)
+	{
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTimeZone(TimeZone.getTimeZone("UTC"));
+		calendar.setTime(from);
+		return calendar;
+	}
+
+	static float fromISO(String iso)
+	{
+		String s = iso.replace("Z", "+00:00");
+		try
+		{
+			s = s.substring(0, 22) + s.substring(23);  // to get rid of the ":"
+		}
+		catch (IndexOutOfBoundsException e)
+		{
+			
+		}
+
+		Date date = null;
+
+		try
+		{
+			date = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ").parse(s);
+		}
+		catch(java.text.ParseException e)
+		{
+
+		}
+		
+		return (float)date.getTime();
+	}
+
+	static Date getDate(float timestamp)
+	{
+		return new Date((long)timestamp);
+	}
 }
--- hypsystem/system/DateTools.hx
+++ hypsystem/system/DateTools.hx
@@ -1,10 +1,93 @@
 package hypsystem.system;
 
-@:build(ShortCuts.mirrors())
 class DateTools
 {
 
-	public static function dateFromIsoString(isoString:String):Date
+	public static function getTimezoneOffset(date:Date):Float
+	{
+		return DateNative.getTimezoneOffset();
+	}
+
+	static inline function getDateTime(date:Date):Float
+	{
+		return date.getTime();
+	}
+
+	public static function getUTCFullYear(date:Date):Float
+	{
+		return DateNative.getUTCFullYear(getDateTime(date));
+	}
+
+	public static function getUTCMonth(date:Date):Float
+	{
+		return DateNative.getUTCMonth(getDateTime(date));
+	}
+
+	public static function getUTCDay(date:Date):Float
+	{
+		return DateNative.getUTCDay(getDateTime(date));
+	}
+
+	public static function getUTCDate(date:Date):Float
+	{
+		return DateNative.getUTCDate(getDateTime(date));
+	}
+
+	public static function getUTCHours(date:Date):Float
+	{
+		return DateNative.getUTCHours(getDateTime(date));
+	}
+
+	public static function getUTCMinutes(date:Date):Float
+	{
+		return DateNative.getUTCMinutes(getDateTime(date));
+	}
+
+	public static function getUTCSeconds(date:Date):Float
+	{
+		return DateNative.getUTCSeconds(getDateTime(date));
+	}
+
+	public static function getUTCMilliseconds(date:Date):Float
+	{
+		return DateNative.getUTCMilliseconds(getDateTime(date));
+	}
+
+	public static function toUTCString(date:Date):String
+	{
+		return DateNative.toUTCString(getDateTime(date));
+	}
+
+	public static function toISOString(date:Date):String
+	{
+		return DateNative.toISOString(getDateTime(date));
+	}
+
+	public static function fromISO(s:String):Date
+	{
+		var t = DateNative.fromISO(s);
+		return Date.fromTime(t);
+	}
+}
+
+@:build(ShortCuts.mirrors())
+class DateNative
+{
+
+	@JNI public static function getTimezoneOffset():Float{}
+	@JNI public static function getUTCFullYear(timestamp:Float):Float{}
+	@JNI public static function getUTCMonth(timestamp:Float):Float{}
+	@JNI public static function getUTCDay(timestamp:Float):Float{}
+	@JNI public static function getUTCDate(timestamp:Float):Float{}
+	@JNI public static function getUTCHours(timestamp:Float):Float{}
+	@JNI public static function getUTCMinutes(timestamp:Float):Float{}
+	@JNI public static function getUTCSeconds(timestamp:Float):Float{}
+	@JNI public static function getUTCMilliseconds(timestamp:Float):Float{}
+	@JNI public static function toUTCString(timestamp:Float):String{}
+	@JNI public static function toISOString(timestamp:Float):String{}
+	@JNI public static function fromISO(s:String):Float{}
+
+	static function dateFromIsoString(isoString:String):Date
 	{
 		var time = fromISOStringToMs(isoString);
 		if(time == -1)
@@ -13,7 +96,7 @@ class DateTools
 		return Date.fromTime(time);
 	}
 
-	public static function dateToIsoString(date:Date):String
+	static function dateToIsoString(date:Date):String
 	{
 		var timestamp = date.getTime();
 		return timestampToIsoString(timestamp);
