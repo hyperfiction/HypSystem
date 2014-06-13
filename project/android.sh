e4ef53c287dc44c3b3929ed6ce0df897501d0b43
shoebox <shoeboks@gmail.com>
2014-06-13 08:53:29 +0100

    Recompiling libs

--- project/android.sh
+++ project/android.sh
@@ -1,8 +1,8 @@
 rm -rf "obj"
 rm -rf "all_objs"
 echo "Compiling for armv6"
-haxelib run hxcpp Build.xml -Dandroid -debug
+haxelib run hxcpp Build.xml -Dandroid
 echo "Compiling for armv7"
-haxelib run hxcpp Build.xml -Dandroid -debug -DHXCPP_ARMV7
+haxelib run hxcpp Build.xml -Dandroid -DHXCPP_ARMV7
 rm -rf "obj"
 rm -rf "all_objs"
